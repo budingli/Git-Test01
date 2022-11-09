fdb5c9b3277ff556cc2b3d3b2e5a35cf5299096f
liyanjie <459398695@qq.com>
2022-11-09 11:10:59 +0800

    master commit3

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello.hot-fix1");
         System.out.println("hello.master test err01");
         System.out.println("hello.hot-fix test err");
+        System.out.println("master push test01");
     }
 }
