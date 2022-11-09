5c4d769230d5f801ef8311d630289c640f316417
liyanjie <459398695@qq.com>
2022-11-09 10:32:58 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello.txt");
         System.out.println("hello.txt2");
+        System.out.println("hello.txt3");
     }
 }
