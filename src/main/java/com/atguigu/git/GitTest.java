d99d01791ad7429f8d381a14bd659597c98748f1
liyanjie <459398695@qq.com>
2022-11-09 10:42:59 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello.txt");
         System.out.println("hello.txt2");
         System.out.println("hello.txt3");
+        System.out.println("hello.hot-fix1");
     }
 }
