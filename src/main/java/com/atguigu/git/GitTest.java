0435d721fedcdcb110ddd7b90b06e647d47cfa87
liyanjie <459398695@qq.com>
2022-11-09 10:28:05 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2022-11-09 10:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.txt");
+    }
+}
